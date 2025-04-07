5be11185080ff143bd3dd19e72e548f440de5eea
bugs macbook-air <rnaldi491@gmail.com>
2025-04-08 07:10:38 +0900

    modify data-table

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from '@/components/appsidebar'
+import { Separator } from '@/components/ui/separator'
 import { Sidebar, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import React from 'react'
 
@@ -8,8 +9,14 @@ const DashboardLayout = ({children}) => {
       <AppSidebar />
       
       <main className='bg-[#08080a] h-screen w-full overflow-y-auto pt-12'>
-        <div className="fixed h-12 top-0 z-50 bg-[#08080a]/80 px-4 backdrop-blur-md w-full border-b border-grid flex items-center">
+        <div className="fixed h-12 top-0 z-50 bg-[#08080a]/80 px-4 backdrop-blur-md w-full border-b border-grid flex items-center gap-4">
             <SidebarTrigger className="text-slate-200" />
+            
+            <div className=' flex flex-row gap-4 w-auto h-6'>
+              <Separator orientation='vertical' className="bg-neutral-700"/>
+              <p className='text-slate-200 font-bold'>Form Distrik</p>
+            </div>
+
         </div>
         <div className='px-4 h-screen w-full overflow-y-auto'>
             {children}
--- components/ui/dropdown-menu.jsx
+++ components/ui/dropdown-menu.jsx
@@ -59,9 +59,11 @@ function DropdownMenuItem({
     <DropdownMenuPrimitive.Item
       data-slot="dropdown-menu-item"
       data-inset={inset}
-      data-variant={variant}
+      // data-variant={variant}
       className={cn(
-        "focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        // yang di bawah ini class yg asli
+        // "focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "focus:bg-neutral-600 focus:text-slate-200 data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 ",
         className
       )}
       {...props} />
